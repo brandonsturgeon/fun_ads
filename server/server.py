851192a8a48838423c41c3aaaa8561ad10bb964f
Brandon Sturgeon <sturgeonb4@gmail.com>
2016-12-19 11:13:43 -0600

    Allows the path to determine which string is found

--- server/server.py
+++ server/server.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-@app.route("/")
-def main_page():
-    return render_template('main.html')
+@app.route("/<text>")
+def main_page(text):
+    return render_template('main.html', text=text)
 
 if __name__ == "__main__":
     app.run(debug=True)
